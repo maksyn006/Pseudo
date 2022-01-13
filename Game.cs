9d19bbd9c93d990c534bf5ed08872eb25495f5ea
maksim penkov <chudnivakk@gmail.com>
2022-01-13 20:28:59 +0200

    Moved test rect and controls to player.cs

--- Game.cs
+++ Game.cs
@@ -14,18 +14,16 @@ namespace Pseudo
         Player player;
         public Game() 
         {
-            player = new Player((float)(Math.PI / 2), 0f, 0.05f,0f);
+            player = new Player((float)(Math.PI / 2), 0f, 0.05f,0f,new FloatRect(0,0,1,1));
             c = new SFML.System.Clock();
-            map = new List<Tile>();
-            map.Insert(0, new Tile( new SFML.Graphics.FloatRect(0,0,1,1), SFML.Graphics.Color.Red  ) );
-            map.Insert(1, new Tile(new SFML.Graphics.FloatRect(0, 100, 150, 150), SFML.Graphics.Color.Green));
-            map.Insert(2, new Tile(new SFML.Graphics.FloatRect(350, 0, 150, 150), SFML.Graphics.Color.Blue));
-            map.Insert(3, new Tile(new SFML.Graphics.FloatRect(500, 300, 150, 150), SFML.Graphics.Color.Yellow));
-            map.Insert(4, new Tile(new SFML.Graphics.FloatRect(200, 300, 100, 100), SFML.Graphics.Color.Black));
+            map = new List<Tile>();            
+            map.Insert(0, new Tile(new SFML.Graphics.FloatRect(0, 100, 150, 150), SFML.Graphics.Color.Green));
+            map.Insert(1, new Tile(new SFML.Graphics.FloatRect(350, 0, 150, 150), SFML.Graphics.Color.Blue));
+            map.Insert(2, new Tile(new SFML.Graphics.FloatRect(500, 300, 150, 150), SFML.Graphics.Color.Yellow));
+            map.Insert(3, new Tile(new SFML.Graphics.FloatRect(200, 300, 100, 100), SFML.Graphics.Color.Black));
         }
         public void Update()
         {
-            PlayerMove();
             time = c.ElapsedTime.AsMicroseconds()/1000;
             float fps = 1f / c.Restart().AsSeconds();
             Program.win.SetTitle(fps.ToString());
@@ -44,43 +42,31 @@ namespace Pseudo
             {
                 Intersect(i);
             }
+            Program.win.Draw(player);
         }
         private void Intersect(float angle)
         {
             float dx = (float)Math.Cos(angle);
             float dy = (float)Math.Sin(angle);
-            //Console.WriteLine("cos " + angle.ToString() + " =  " + dx.ToString());
-            //Console.WriteLine("sin " + angle.ToString() + " =  " + dy.ToString());
-            float startLeft = map.ElementAt(0).fr.Left;
-            float startTop = map.ElementAt(0).fr.Top;
-            for (int i = 1; i < map.Count; i++)
+            float startLeft = player.playerRect.Left;
+            float startTop = player.playerRect.Top;
+            for (int i = 0; i < map.Count; i++)
             {
-                while (!map.ElementAt(0).fr.Intersects(map.ElementAt(i).fr) && (Math.Abs(map.ElementAt(0).fr.Left - map.ElementAt(i).fr.Left)<800) && (Math.Abs(map.ElementAt(0).fr.Top - map.ElementAt(i).fr.Top) < 800))
+                while (!player.playerRect.Intersects(map.ElementAt(i).fr) && (Math.Abs(player.playerRect.Left - map.ElementAt(i).fr.Left)<800) && (Math.Abs(player.playerRect.Top - map.ElementAt(i).fr.Top) < 800))
                 {
-                    map.ElementAt(0).fr.Left += dx;
-                    map.ElementAt(0).fr.Top += dy;
+                    player.playerRect.Left += dx;
+                    player.playerRect.Top += dy;
                 }
-                if(map.ElementAt(0).fr.Intersects(map.ElementAt(i).fr))
+                if(player.playerRect.Intersects(map.ElementAt(i).fr))
                 {
                     RectangleShape rect = new RectangleShape(new SFML.System.Vector2f(5,5));
-                    rect.Position = new SFML.System.Vector2f(map.ElementAt(0).fr.Left, map.ElementAt(0).fr.Top);
+                    rect.Position = new SFML.System.Vector2f(player.playerRect.Left, player.playerRect.Top);
                     rect.FillColor = map.ElementAt(i).c;
                     Program.win.Draw(rect);
                 }
-                map.ElementAt(0).fr.Left = startLeft;
-                map.ElementAt(0).fr.Top = startTop;            
+                player.playerRect.Left = startLeft;
+                player.playerRect.Top = startTop;            
             }
-            RectangleShape test = new RectangleShape(new SFML.System.Vector2f(5, 5));
-            test.Position = new SFML.System.Vector2f(map.ElementAt(0).fr.Left, map.ElementAt(0).fr.Top);
-            test.FillColor = map.ElementAt(0).c;
-            Program.win.Draw(test);
         }
-        private void PlayerMove(){
-            if (SFML.Window.Keyboard.IsKeyPressed(SFML.Window.Keyboard.Key.A)) map.ElementAt(0).fr.Left -= 0.1f*time;
-            if (SFML.Window.Keyboard.IsKeyPressed(SFML.Window.Keyboard.Key.D)) map.ElementAt(0).fr.Left += 0.1f*time;
-            if (SFML.Window.Keyboard.IsKeyPressed(SFML.Window.Keyboard.Key.W)) map.ElementAt(0).fr.Top -= 0.1f*time;
-            if (SFML.Window.Keyboard.IsKeyPressed(SFML.Window.Keyboard.Key.S)) map.ElementAt(0).fr.Top += 0.1f*time;
-        }
-
     }
 }
--- Player.cs
+++ Player.cs
@@ -1,4 +1,5 @@
-﻿using SFML.Window;
+﻿using SFML.Graphics;
+using SFML.Window;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,19 +8,17 @@ using System.Threading.Tasks;
 
 namespace Pseudo
 {
-    internal class Player
+    internal class Player:Transformable,Drawable
     {
-        public float fov_scale;
-        public float fov;
-        public float fov_speed;
-        public float speed;
-        public float time;
-        public Player(float fov_scale,float fov,float fov_speed,float speed)
+        public float fov_scale, fov, fov_speed, speed, time;
+        public FloatRect playerRect;
+        public Player(float fov_scale,float fov,float fov_speed,float speed,FloatRect playerRect)
         {
             this.fov_scale = fov_scale;
             this.fov = fov;
             this.speed = speed;
             this.fov_speed = fov_speed;
+            this.playerRect = playerRect;
         }
         public void Update(float time)
         {
@@ -29,6 +28,18 @@ namespace Pseudo
         public void Control() {
             if (Keyboard.IsKeyPressed(Keyboard.Key.Z)) fov += (float)(Math.PI / 180)*time*fov_speed;
             if (Keyboard.IsKeyPressed(Keyboard.Key.X)) fov -= (float)(Math.PI / 180)*time*fov_speed;
+            if (SFML.Window.Keyboard.IsKeyPressed(SFML.Window.Keyboard.Key.A)) playerRect.Left -= 0.1f * time;
+            if (SFML.Window.Keyboard.IsKeyPressed(SFML.Window.Keyboard.Key.D)) playerRect.Left += 0.1f * time;
+            if (SFML.Window.Keyboard.IsKeyPressed(SFML.Window.Keyboard.Key.W)) playerRect.Top -= 0.1f * time;
+            if (SFML.Window.Keyboard.IsKeyPressed(SFML.Window.Keyboard.Key.S)) playerRect.Top += 0.1f * time;
+
+        }
+        public void Draw(RenderTarget target, RenderStates states)
+        {
+            RectangleShape rect = new RectangleShape(new SFML.System.Vector2f(5, 5));
+            rect.Position = new SFML.System.Vector2f(playerRect.Left, playerRect.Top);
+            rect.FillColor = Color.Red;
+            target.Draw(rect);
         }
     }
 }
